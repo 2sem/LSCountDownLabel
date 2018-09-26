4f02e4878525997c190089c1f708bc49213aaf36
leesam <kofsiwon@siwonschool.com>
2018-09-26 10:36:46 +0900

    Initial Commit

---
+++ LSCountDownLabel/LSCountDownLabel.h
@@ -0,0 +1,19 @@
+//
+//  LSCountDownLabel.h
+//  LSCountDownLabel
+//
+//  Created by 영준 이 on 2018. 9. 26..
+//  Copyright © 2018년 Y2KLab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LSCountDownLabel.
+FOUNDATION_EXPORT double LSCountDownLabelVersionNumber;
+
+//! Project version string for LSCountDownLabel.
+FOUNDATION_EXPORT const unsigned char LSCountDownLabelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LSCountDownLabel/PublicHeader.h>
+
+
